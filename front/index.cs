ee76317c80b0547a390f9ed0a13d6b0a04ccbbb1
MazzCamacho23 <mazzalejocamacho@gmail.com>
2025-12-23 11:33:15 -0500

    maquetacion

---
+++ front/index.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace Conswareencuesta.front
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
